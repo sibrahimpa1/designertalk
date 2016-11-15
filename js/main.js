06e1e79ad20dbd648b572834e6d7154502e3d958
Senka <sibrahimpa1@etf.unsa.ba>
2016-11-15 22:06:14 +0100

    ajax

---
+++ js/main.js
@@ -0,0 +1,84 @@
+window.onload = function() {
+  loadMain();
+};
+
+function loadPart(content, where) {
+
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(where).innerHTML +=
+        this.responseText;
+    }
+  };
+  xhttp.open("GET", content, true);
+  xhttp.send();
+}
+
+function loadMain() {
+  document.getElementById("main-page").innerHTML = "";
+
+  loadPart("ajax/join-us-box.html", "main-page");
+  loadPart("ajax/main-page.html", "main-page");
+  loadPart("ajax/design-box.html", "design-wrap");
+  loadPart("ajax/forum-box.html", "forum-wrap");
+}
+
+function loadProfile() {
+  document.getElementById("main-page").innerHTML = "";
+
+  loadPart("ajax/profile.html", "main-page");
+  loadPart("ajax/top-profile-info.html", "profile-wrap");
+  loadPart("ajax/design-box.html", "design-content");
+  loadPart("ajax/forum-box.html", "forum-content");
+}
+
+function loadDesigns() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/design-list.html", "main-page");
+  loadPart("ajax/design-box.html", "content-box");
+}
+
+function loadDesignPost() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/design-post.html", "main-page");
+  loadPart('ajax/big-design-box.html', "content-box");
+  loadPart('ajax/comment-box.html', "post-page");
+
+}
+
+function loadForum() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/forum.html", "main-page");
+  loadPart("ajax/forum-box.html", "content-box");
+}
+
+function loadForumPost() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/forum-post.html", "main-page");
+  loadPart('ajax/forum-box.html', "content-box");
+  loadPart('ajax/comment-box.html', "post-page");
+
+}
+
+function loadAbout() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/about.html", "main-page");
+}
+
+function loadContact() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/contact.html", "main-page");
+}
+
+function loadLogin() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/login.html", "main-page");
+}
+
+
+function loadRegister() {
+  document.getElementById("main-page").innerHTML = "";
+  loadPart("ajax/register.html", "main-page");
+}
