de8fb8af8d5942704514d6b5489fec5f666e7ee2
Senka <sibrahimpa1@etf.unsa.ba>
2016-11-21 17:31:11 +0100

    Spirala 2 final

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# 
+#
 
 Designers talk
 
 Forum za web dizajnere, graficke i logo dizajnere, ilustratore i ostale kreativce. Designers talk je mjesto za istrazivanje i promovisanje dizajna.
 
 Spirala 1.
-  - Svi zadaci su urađeni, nisam primjetila ni jedan bug.
+  - Svi zadaci su urađeni, nisam primjetila nijedan bug.
   - Lista fajlova:
-   - index.html - početna stranica sa nekim osnovnim sadržajem 
+   - index.html - početna stranica sa nekim osnovnim sadržajem
    - about.html - stranica na kojoj treba pisati nešto više o nama
    - contact.html - stranica sa kontakt formom
    - design-list.html - lista svih dizajnova, sa opcijama sortiranja
@@ -16,7 +16,47 @@ Spirala 1.
    - forum-post.html - detaljniji prikaz posta na forumu
    - login.html i register.html - stranice sa formama login i registraciju
    - profile.html - stranica profila korisnika
-    
+
    - Css fajlovi su razvrstani i imenovani po stranicama, u main.css-u se nalazi sav css potreban za elemente koji se pojavljuju na više      stranica.
+
+
+Spirala 2.
+
+  - Sva polja formi imaju odgovarajuću JavaScript validaciju, poruka o grešci se ispisuje iznad submit buttona
+  - Implementiran JS dropdown meni u headeru na mobilnim verzijama
+  - Pošto će kroz cijelu stranicu biti jakno puno prikazanih 'design-box' elemenata implementirano je otvaranje modala klikom na sliku na design-boxu (gdje god ima prikaz dizajna na stranici osim na design-post podstranici gdje je slika već velikih dimenzija). Modal se može ugasiti na 'esc' dugme ili klikom na dugme u gornjem desnom ćošku.
+  - Sve podstranice (kojima pristupamo iz headera ili pomoću 'see-more' linkova), te i neki elementi unutar tih podstranica, se učitavaju ajaxom
+  - Nisam primjetila nijedan bug.
+  - HTML fajlovi iz prethodne spirale su modifikovani i sada se nalaze u ajax folderu.
+
+  - Lista fajlova:
+
+   - about.html - svi elementi podstranice 'o nama'
+   - big-design-box - veliki box koji se prikazuje samo na podstranici jednog dizajna
+   - comment-box - prikazuje se na podstranici jednog dizajna i jednog forum posta
+   - contact.html - svi elementi podstranice sa kontakt formom
+   - design-box.html - prikaz malog design-boxa koji se loada na main page-u i u listi svi dizajnova
+   - design-list.html - kontejner unutar kojeg ce biti izlistani svi boxovi dizajna na podstranici 'Designs' i 'Main page'
+   - design-post.html - kontejner za veliki design box -> ovdje se ucitava 'big-design-box' i 'comment-box'
+
+   - forum-post.html - kontejner za prikaz jednog forum boxa -> ovdje se ucitava 'forum-box' i 'comment-box'
+   - forum-box.html - prikaz forum-boxa : na main page-u, u listi svi postova na forumu (podstranica 'Forum') i u podstranici jednog forum posta
+   - forum.html - kontejner za listu svih postova na forumu
+
+   - join-us-box.html - ucitava se na main stranici na samom pocetku stranice
+   - login.html - svi elementi podstranice 'login'
+   - main-page.html - kontejner za ucitavanje design-posta i forum-posta na pocetnoj stranici ispod join-us-boxa
+   - profile.html - kontejner za ucitavanje top-profile-info boxa, design-boxa, i forum-boxa
+   - top-profile-info.html - box koji sadrzi osnovne informacije o svakom profilu - slika, ime, opis, broj forum postova itd.
+   - register.html - svi elementi podstranice 'register'
+
+
+   - formValidation.js - validacija svih formi na stranici
+   - main.js - ucitavanje svih podstranica ajaxom, dropdown meni, modal
+
+   - index.html - pocetna stranica
+
+
+
 
 Ibrahimpasic Senka, 16511
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 window.onload = function() {
-  loadMain();
+  loadMain(); // starting point
 };
+
+
+// otvara modal klikom na sliku (design-box)
 
 function toggleModal(type) {
   var tmp = document.getElementById("design-img").getAttribute("src");
@@ -14,16 +17,19 @@ function toggleModal(type) {
   }
 }
 
+// gasi modal na esc
 window.onkeydown = function(e) {
   if (e.keyCode === 27) {
     toggleModal('hide');
   }
 }
 
+// header dropdown list - na mobilnoj verziji
 function dropdownToggle() {
   document.getElementById("dropdown").classList.toggle('show-menu');
 }
 
+// hide dropdown kad se klikne bilo gdje na stranici osim na link
 window.onclick = function() {
   if (!event.target.matches('.open-dropdown') && !event.target.matches('.dropdown ul li a')) {
     if (document.getElementById("dropdown").classList.contains('show-menu')) {
@@ -32,16 +38,17 @@ window.onclick = function() {
   }
 }
 
+// ajax call za svaku podstranicu/element na stranici
 function loadPart(content, where, cb) {
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    console.log(document.getElementById(where).innerHTML);
+
       document.getElementById(where).innerHTML +=
         this.responseText;
 
-      if(typeof cb === "function"){
+      if (typeof cb === "function") {
         cb();
       }
     }
@@ -51,53 +58,62 @@ function loadPart(content, where, cb) {
   xhttp.send();
 }
 
+// svaka f-ja posebno loada potrebne dijelove stranice
 
 function loadMain() {
   document.getElementById("main-page").innerHTML = "";
 
-  loadPart("ajax/join-us-box.html", "main-page",
-    loadPart("ajax/main-page.html", "main-page",
-      loadPart("ajax/design-box.html", "design-wrap",
+  loadPart("ajax/join-us-box.html", "main-page", function() {
+    loadPart("ajax/main-page.html", "main-page", function() {
+      loadPart("ajax/design-box.html", "design-wrap", function() {
         loadPart("ajax/forum-box.html", "forum-wrap")
-      )
-    )
-  );
+      })
+    })
+  });
 }
 
 function loadProfile() {
   document.getElementById("main-page").innerHTML = "";
 
-  loadPart("ajax/profile.html", "main-page");
-  loadPart("ajax/top-profile-info.html", "profile-wrap");
-  loadPart("ajax/design-box.html", "design-content");
-  loadPart("ajax/forum-box.html", "forum-content");
+  loadPart("ajax/profile.html", "main-page", function() {
+    loadPart("ajax/top-profile-info.html", "profile-wrap", function() {
+      loadPart("ajax/design-box.html", "design-content", function() {
+        loadPart("ajax/forum-box.html", "forum-content")
+      })
+    })
+  });
 }
 
 function loadDesigns() {
   document.getElementById("main-page").innerHTML = "";
-  loadPart("ajax/design-list.html", "main-page");
-  loadPart("ajax/design-box.html", "content-box");
+  loadPart("ajax/design-list.html", "main-page", function() {
+    loadPart("ajax/design-box.html", "content-box")
+  });
 }
 
 function loadDesignPost() {
   document.getElementById("main-page").innerHTML = "";
-  loadPart("ajax/design-post.html", "main-page");
-  loadPart('ajax/big-design-box.html', "content-box");
-  loadPart('ajax/comment-box.html', "post-page");
-
+  loadPart("ajax/design-post.html", "main-page", function() {
+    loadPart('ajax/big-design-box.html', "content-box", function() {
+      loadPart('ajax/comment-box.html', "post-page")
+    })
+  });
 }
 
 function loadForum() {
   document.getElementById("main-page").innerHTML = "";
-  loadPart("ajax/forum.html", "main-page");
-  loadPart("ajax/forum-box.html", "content-box");
+  loadPart("ajax/forum.html", "main-page", function() {
+    loadPart("ajax/forum-box.html", "content-box")
+  });
 }
 
 function loadForumPost() {
   document.getElementById("main-page").innerHTML = "";
-  loadPart("ajax/forum-post.html", "main-page");
-  loadPart('ajax/forum-box.html', "content-box");
-  loadPart('ajax/comment-box.html', "post-page");
+  loadPart("ajax/forum-post.html", "main-page", function() {
+    loadPart('ajax/forum-box.html', "content-box", function() {
+      loadPart('ajax/comment-box.html', "post-page")
+    })
+  });
 
 }
 
@@ -115,7 +131,6 @@ function loadLogin() {
   document.getElementById("main-page").innerHTML = "";
   loadPart("ajax/login.html", "main-page");
 }
-
 
 function loadRegister() {
   document.getElementById("main-page").innerHTML = "";
